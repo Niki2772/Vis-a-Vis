eed13d084d06bed1033c37ea28727d495df1b53e
Niki Klokova <klokova.nika17@gmail.com>
2020-12-08 16:12:32 +0200

    progress bar

--- public/assets/js/progressBar.js
+++ public/assets/js/progressBar.js
@@ -2,13 +2,16 @@ let prBarFirst = document.getElementById("prBarFirst");
 let prBarSecond = document.getElementById("prBarSecond");
 let prBarThird = document.getElementById("prBarThird");
 let prBarFourth = document.getElementById("prBarFourth");
+let progressBarOffset = document.getElementById("progressBarBlock").offsetTop;
 
 let i = 0;
+
 function move(element) {
     if (i === 0) {
         i = 1;
         let width = 1;
         let id = setInterval(frame, 10);
+
         function frame() {
             if (width >= 100) {
                 clearInterval(id);
@@ -21,10 +24,8 @@ function move(element) {
     }
 }
 
-let progressBarOffset = document.getElementById("progressBarBlock").offsetTop;
-
-window.addEventListener('scroll', function() {
-    if(window.pageYOffset == progressBarOffset){
+window.addEventListener('scroll', function () {
+    if (window.pageYOffset == progressBarOffset) {
         setTimeout(move, 1000, prBarFirst);
         setTimeout(move, 2000, prBarSecond);
         setTimeout(move, 3000, prBarThird);
